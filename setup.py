0330eab0dfddd754b878dd98916cc17fea4fc696
David D. Riddle <ddriddle@illinois.edu>
2022-01-22 18:48:12 -0600

    Modernize build.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 def version():
@@ -14,42 +14,6 @@ def version():
 
 
 setup(
-    name="farmit",
     use_scm_version=version,
-    setup_requires=['setuptools_scm'],
-    author="David D. Riddle",
-    author_email="securitysupport@illinois.edu",
-    description="Fully Automated Release Management tool",
-    long_description_content_type="text/markdown",
-    url="https://github.com/techservicesillinois/farmit",
-    packages=find_packages('src', exclude=['tests']),
-    package_dir={'': 'src'},
-    extras_require={
-        'testing': ['pytest'],
-    },
-    install_requires=[
-        "GitPython",
-        "pydriller",
-        "setuptools_scm",
-    ],
-    entry_points={
-        'console_scripts': [
-            'farmit = farmit:_main',
-        ],
-    },
-    include_package_data=True,
-    classifiers=[
-        #  'Development Status :: 4 - Beta',
-        "Operating System :: OS Independent",
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 3",
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-    python_requires='>=3.6',
+    setup_requires=['setuptools_scm', 'wheel'],
 )
